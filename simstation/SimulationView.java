8ba34c81a78a7307fa27b3612fdc045056784b59
Adarsh <amlearning2023@gmail.com>
2024-04-12 12:50:15 -0700

    Removed update(), added paintComponent() to SimulationView class

--- simstation/SimulationView.java
+++ simstation/SimulationView.java
@@ -1,13 +1,15 @@
 package simstation;
 import mvc.*;
 
+import java.awt.*;
+
 public class SimulationView extends View {
 
     public SimulationView(Model m){
         super(m);
     }
 
-    public void update(){
-
+    public void paintComponent(Graphics gc){
+        // implementation
     }
 }
