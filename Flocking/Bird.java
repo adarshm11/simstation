4b7cedc24cfb2f500f0c011d2e1f0691acbb2db0
Adarsh <amlearning2023@gmail.com>
2024-04-15 11:26:42 -0700

    Created files for flocking simulation

---
+++ Flocking/Bird.java
@@ -0,0 +1,11 @@
+package Flocking;
+import simstation.*;
+
+public class Bird extends Agent {
+
+    protected int speed;
+
+    public void update(){
+
+    }
+}
---
+++ Flocking/FlockingSimulation.java
@@ -0,0 +1,16 @@
+package Flocking;
+import simstation.*;
+
+public class FlockingSimulation extends Simulation {
+
+
+    public void populate(){
+
+    }
+
+    public String[] getStats(){
+        return new String[]{};
+    }
+
+
+}
