7a51c3aa51c3f4b38adbfaf3da42c22003da93eb
VatsPanchal <101433805+VatsPanchal@users.noreply.github.com>
2024-04-16 18:07:55 -0700

    implemented updateView in prisoner dilemma view

--- PrisonersDilemma/PrisonerDilemmaView.java
+++ PrisonersDilemma/PrisonerDilemmaView.java
@@ -25,6 +25,39 @@ public class PrisonerDilemmaView extends View {
 
     @Override
     public void updateView() {
-        // Update displayPanel and statisticsLabel based on model data
+        for (Prisoner prisoner : model.getPrisoners()) {
+            JLabel prisonerLabel = new JLabel(prisoner.toString());
+            displayPanel.add(prisonerLabel);
+        }
+        
+        // Repaint the displayPanel
+        displayPanel.revalidate();
+        displayPanel.repaint();
+        
+        // Update statisticsLabel
+        String statistics = generateStatistics(); 
+        statisticsLabel.setText(statistics);
+    }
+    
+    private String generateStatistics() {
+        StringBuilder statistics = new StringBuilder();
+        statistics.append("Average Fitness:\n");
+        for (Strategy strategy : model.getStrategies()) {
+            int averageFitness = calculateAverageFitnessForStrategy(strategy);
+            statistics.append(strategy.getClass().getSimpleName()).append(": ").append(averageFitness).append("\n");
+        }
+        return statistics.toString();
+    }
+    
+    private int calculateAverageFitnessForStrategy(Strategy strategy) {
+        int totalFitness = 0;
+        int count = 0;
+        for (Prisoner prisoner : model.getPrisoners()) {
+            if (prisoner.getStrategy().equals(strategy)) {
+                totalFitness += prisoner.getFitness();
+                count++;
+            }
+        }
+        return count > 0 ? totalFitness / count : 0;
     }
 }
